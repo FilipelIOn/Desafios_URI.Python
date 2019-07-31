dc55cd89382bafef3d332e08c2b2d8555ed9e5f2
filipe <filipe.freitas@outlook.com>
2019-07-31 10:05:56 -0300

    Novos problemas

--- Contests/Roteiro UFMG/Roteiro 0/URI1534.py
+++ Contests/Roteiro UFMG/Roteiro 0/URI1534.py
@@ -1,5 +1,21 @@
 while True:
     try:
+        x = 0
+        tam = int(input())
+        for i in range(tam):
+            linha = []
+            for j in range(tam):
+                if (j == x) and (j == (tam - x - 1)):
+                    linha.append(2)
+                elif j == x:
+                    linha.append(1)
+                elif j == (tam - x - 1):
+                    linha.append(2)
+                else:
+                    linha.append(3)
+            [print(p, end="") for p in linha]
+            print()
+            x += 1
 
     except EOFError:
         break
