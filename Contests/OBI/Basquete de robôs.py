d8996c04dd1894895db70adc478f614accad6dff
filipelion <filipe.freitas@outlook.com>
2019-09-27 15:28:55 -0300

    Problemas da OBI

---
+++ Contests/OBI/Basquete de robôs.py
@@ -0,0 +1,8 @@
+d = int(input())
+
+if d < 801:
+    print(1)
+if d > 800 and d < 1401:
+    print(2)
+elif d > 1400 and d < 2001:
+    print(3)
---
+++ Contests/OBI/Cápsulas.py
@@ -0,0 +1,3 @@
+n, f = map(lambda x: int(x), input().split())
+coins = list(map(lambda x: int(x), input().split()))
+print(coins)
