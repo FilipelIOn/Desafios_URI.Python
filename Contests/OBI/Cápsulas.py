0b1c5262bc96c97eb449fb4111d097ac676f0f41
filipelion <filipe.freitas@outlook.com>
2019-09-30 20:43:33 -0300

    binary search in cápsulas

--- Contests/OBI/Cápsulas.py
+++ Contests/OBI/Cápsulas.py
@@ -1,14 +1,19 @@
 n, f = map(lambda x: int(x), input().split())
-coins = list(map(lambda x: int(x), input().split()))
+cicles = list(map(lambda x: int(x), input().split()))
+min_day, max_day = 1, 10**9
 
-d = 1
-while True:
-    x = 0
-    for i in coins:
-        x += int(d/i)
+#binary search
+while min_day < max_day:
+    mid = int((max_day+min_day)/2)
+    total = 0
 
-    if x >= f:
-        break
-    d += 1
+    for i in cicles:
+        total += int(mid/i)
 
-print(d)
+    if total >= f:
+        max_day = mid
+    else:
+        min_day = mid + 1
+    print(min_day,max_day)
+
+print(min_day)
