333b58c288e1df4596887ec8a925c7baa3b385b1
filipelion <filipe.freitas@outlook.com>
2019-09-30 15:16:53 -0300

    Problema Cápsulas terminado

--- Contests/OBI/Cápsulas.py
+++ Contests/OBI/Cápsulas.py
@@ -1,3 +1,14 @@
 n, f = map(lambda x: int(x), input().split())
 coins = list(map(lambda x: int(x), input().split()))
-print(coins)
+
+d = 1
+while True:
+    x = 0
+    for i in coins:
+        x += int(d/i)
+
+    if x >= f:
+        break
+    d += 1
+
+print(d)
